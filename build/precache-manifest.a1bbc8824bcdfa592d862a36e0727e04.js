bce6f50013bc036cd0084bd2bbd2662a69dc0985
jasper <msavantfou@gmail.com>
2020-04-16 11:59:12 +1000

    k

---
+++ build/precache-manifest.a1bbc8824bcdfa592d862a36e0727e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30faaa23f200f51b9eea94139aa5fed3",
+    "url": "/Jasper8628/react_week19/index.html"
+  },
+  {
+    "revision": "7bf22ea6a547965df9e2",
+    "url": "/Jasper8628/react_week19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d035e3d85b5d8961532e",
+    "url": "/Jasper8628/react_week19/static/js/2.4a777a31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jasper8628/react_week19/static/js/2.4a777a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf22ea6a547965df9e2",
+    "url": "/Jasper8628/react_week19/static/js/main.2c91e1ce.chunk.js"
+  },
+  {
+    "revision": "4209a5f21006c5004c11",
+    "url": "/Jasper8628/react_week19/static/js/runtime-main.c6520a79.js"
+  }
+]);
